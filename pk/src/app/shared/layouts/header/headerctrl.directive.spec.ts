6d67f080b5fa5f4f90f38eecc861b54ad8368a95
Sanjay Kumar <sk963070@gmail.com>
2023-01-03 23:04:05 +0530

    new build

--- pk/src/app/shared/layouts/header/headerctrl.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HeaderctrlDirective } from './headerctrl.directive';
-
-describe('HeaderctrlDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HeaderctrlDirective();
-    expect(directive).toBeTruthy();
-  });
-});
